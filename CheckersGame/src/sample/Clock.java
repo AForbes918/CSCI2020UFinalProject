f325d217a29dbc16fc8cc7aeea14da9aa50c288e
FredeEmnetu <47097318+FredeEmnetu@users.noreply.github.com>
2020-03-23 14:43:27 -0400

    Clock class used for timer

---
+++ CheckersGame/src/sample/Clock.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
+public class Clock extends Pane {
+    private Timeline animation;
+    private int tap = 0;
+    private String s = "";
+
+    Label label = new Label("0");
+
+    Clock(){
+        label.setFont(javafx.scene.text.Font.font(20));
+        getChildren().add(label);
+        animation = new Timeline(new KeyFrame(Duration.seconds(1), e->timeLabel()));
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play();
+    }
+    public void transX(double xmove){
+         label.setTranslateX(xmove);
+    }
+    public void transY(double ymove){
+         label.setTranslateX(ymove);
+    }
+
+
+    private void timeLabel(){
+
+        if (tap > -1) {
+            tap++;
+        }
+            s = tap + "";
+            label.setText(s);
+
+    }
+}
