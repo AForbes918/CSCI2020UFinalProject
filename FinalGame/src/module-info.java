e682a989f50d49d1ca2f22fbe6b92c3de2092058
dennisdeleon06 <dennisalfred.deleon@ontariotechu.net>
2020-03-24 16:55:52 -0400

    Cleaning repository
    Deleted unnecessary files, renamed folder for clarity.

---
+++ FinalGame/src/module-info.java
@@ -0,0 +1,12 @@
+module NEW {
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires transitive javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.swt;
+	requires javafx.web;
+	requires transitive java.compiler;
+	opens WindowTest;
+}
--- finalprojecttest/src/WindowTest/Controller.java
+++
@@ -1,5 +0,0 @@
-package WindowTest;
-
-public class Controller {
-
-}
